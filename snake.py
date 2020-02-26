85047caadaeaafa5e50eb81a21dc9e30a798145c
sheil mehta <sheil.mehta9@gmail.com>
2020-02-25 20:20:56 -0800

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+
+window.title("Snake Game")
+window.bgcolor("black")
+window.setup(width=500 , height= 500)
+window.tracer(0)
+
+
+
+
+
+window.mainloop()
